bc454fe7ff9bca073f1d3c2eab89d238e39074fc
Chhavi-Pandey <157047212+Chhavi-Pandey@users.noreply.github.com>
2025-02-21 04:16:51 +0000

    datatype

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,14 @@
+"use strict";    // treat all js code as the newer version of js; now by default it is always in newer version without even writing "use strict" but still it's a good practice and to know
+
+// Using semi-colon is sometimes needed sometimes not some says using this is a good practice some says it is not. Overall the purpose is only improve the readability of code
+
+// Data Types: number, bigint, string, boolean, null, undefined,symbol
+// We've discussed about undefined and also aware with number, boolean, and string and also bigint as it only used when the number is so big.    
+// null: standalone value and of course it is a type.(empty value)
+// symbol : unique
+
+
+// object (non primitive)
+
+console.log(typeof null) // typeof used for finding the type of value 
+// and here is the typeof null which is object; (and it is interesting)
