fb384ca51177133bc26deb73fb7c04838f838894
Chhavi-Pandey <157047212+Chhavi-Pandey@users.noreply.github.com>
2025-02-21 06:34:44 +0000

    datatype-summary

---
+++ 01_basics/datatype-Summary.js
@@ -0,0 +1,16 @@
+// Primitive data type : Call by value
+
+// 7 types:  String, Boolean, null, undefined, Number, Symbol, BigInt(Scientific value)
+
+// Dynamically typed language
+
+// reference type or Non-Primitive : Call by reference
+
+// Array, Objects, Functions
+
+const id = Symbol('123') // USe for making unique
+
+const bigNumber = 674927479274n
+console.log(typeof bigNumber) // Just use n in the last and it become bigint
+
+// Array use square brackets [], object : in form of key-value pair use {} : Non primitive always object datatype
