09f70dc16996a047f468d50251cb6bf19c16c3db
Chhavi-Pandey <157047212+Chhavi-Pandey@users.noreply.github.com>
2025-02-23 18:55:55 +0000

    stack&heap

--- 01_basics/datatype-Summary.js
+++ 01_basics/datatype-Summary.js
@@ -14,3 +14,31 @@ const bigNumber = 674927479274n
 console.log(typeof bigNumber) // Just use n in the last and it become bigint
 
 // Array use square brackets [], object : in form of key-value pair use {} : Non primitive always object datatype
+
+
+
+// ***********************************************************************************************************************
+// Stack(Primitive)=> gives copy   Heap(non- primitive)=> gives reference
+
+let myFavChar = "Klaus Mikaelson";
+let favTvdChar = myFavChar;
+
+console.log(myFavChar);
+console.log(favTvdChar);
+
+favTvdChar = "Damon Salvatore";
+
+console.log(myFavChar);
+console.log(favTvdChar);
+
+let userOne = {
+    email: "haylay@yahoo.com",
+    upiId : "hay@apl"
+}
+
+let userTwo = userOne;
+
+userTwo.email = "hope@google.com"
+console.log(userOne.email);
+console.log(userTwo.email);
+
