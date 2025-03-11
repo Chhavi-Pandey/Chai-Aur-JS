5e4a06dac33a087b768452db5fafb6b6d6103fac
Chhavi-Pandey <157047212+Chhavi-Pandey@users.noreply.github.com>
2025-03-11 11:33:06 +0000

    object part2 done

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,22 @@
+// const tinderUser = new Object() //singleton object
+const tinderUser = {}
+// console.log(tinderUser);
+
+tinderUser.id = "123abc"
+tinderUser.name = "Bonnie"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "bonnie2512@gmail.com",
+    fullname:{
+        userFullName:{
+            firstName: "Bonnie",
+            lastName: "Bennett"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userFullName.firstName);
+
