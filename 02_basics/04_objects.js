ca88ce4fd57fb82be4d569b175ef480e7efa1a68
Chhavi-Pandey <157047212+Chhavi-Pandey@users.noreply.github.com>
2025-03-11 12:04:09 +0000

    object done

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -20,3 +20,39 @@ const regularUser = {
 
 // console.log(regularUser.fullname.userFullName.firstName);
 
+const obj1 = {
+    1: "a",
+    2: "b"
+}
+
+const obj2 = {3: "c", 4: "d"}
+
+// const obj3 = {obj1, obj2};
+// const obj3 = Object.assign({}, obj1, obj2); // Object.assign(target, source) : so here {} is a target so it is up to you.
+//OR
+const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+
+const users = [
+    {
+        id: 1,
+        email: "c@gmail.com"
+    },
+    {
+        id: 1,
+        email: "c@gmail.com"
+    },
+    {
+        id: 1,
+        email: "c@gmail.com"
+    }
+]
+
+users[1].email;
+
+console.log(tinderUser);
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
