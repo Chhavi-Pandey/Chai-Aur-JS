b57dc232c679522cbea62d26d0163c630aa3cb6c
Chhavi Pandey <pandeychhavi29@gmail.com>
2025-04-10 19:44:04 +0530

    let and var clear

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -4,12 +4,35 @@ var accountPassword = "Chhavi@2003";
 accountCity = " Ayodhya ";  // Not preferable
 // accountId = 2; not allowed as it is const
 // If we don't give any value to a variable then it is undefined
-console.log(accountId);
+// console.log(accountId);
 
 accountEmail = "chhavi29@gmail.com";
 accountPassword = "Chh@2003";
 accountCity = "Mumbai";
 
-// var has scope issues: block scope and functional scope
+// var has scope issues: block scope and functional scope so don't prefer it beacuse it has not any block scope 
 
-console.table([accountEmail, accountId, accountPassword, accountCity]);
+// console.table([accountEmail, accountId, accountPassword, accountCity]);
+
+
+// var chhPandey = 22
+
+// if (true) {
+//     var chhPandey = 30
+//     // console.log(chhPandey); 
+// }
+
+// chhPandey = chhPandey + 10;
+// console.log(chhPandey); // 40
+
+// let age = 22;
+// if (true) {
+//     let age = 24;
+//     console.log(age+5); // 29
+    
+// }
+
+// age = age + 10;
+// console.log(age); //32
+
+// Now I hope this confusion is clear
