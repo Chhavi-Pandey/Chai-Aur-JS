e6e77efec28606f40bd4866746fcfd1b2957d1bb
Chhavi Pandey <pandeychhavi29@gmail.com>
2025-04-10 20:06:55 +0530

    conversion

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -6,10 +6,14 @@ let score = "33abc"
 let valueInNumber = Number(score); // conversion in number using Number (first letter is capital)
 // console.log(typeof valueInNumber)
 
-// console.log(valueInNumber); // NaN : Not a number when we write score's value as "33abc" for null it will be 0 for undefined it will be NaN for boolean value true or false it will be 1 or 0
+// console.log(valueInNumber); // NaN : Not a number when we write score's value as "33abc" ; for null it will be 0; for undefined it will be NaN; for boolean value true or false it will be 1 or 0
 
 // Conversion can be done for boolean (Boolean) string (String) etc.
-
+let isLoggedIn = "";
+let booleanLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanLoggedIn);
+// 1 => true; 0 => false;
+// "" => false; "chhavi" => true
 
 // ***********************************************************Operations*********************************************************
 
