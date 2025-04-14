958ea4e0c224700fad78e5804d0043b2a0a03e4d
Chhavi Pandey <pandeychhavi29@gmail.com>
2025-04-14 10:45:59 +0530

    object basic 2 done

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -26,3 +26,22 @@ const course ={
     courseInstructor : "Hitesh Choudhary" 
 }
 
+const obj1 = {1:"a", 2:"b"};
+const obj2 = {3:"a", 4:"b"};
+
+// const obj3 = {obj1, obj2} //Object inside object
+
+// const obj3 = Object.assign({}, obj1, obj2); 
+//Static methond: Copy the values of all of the enumerable own properties from one or more source objects to a target object. Returns the target object.
+// Can be more sources than two
+// @param target — The target object to copy to.
+// @param source1 — The first source object from which to copy properties.
+// @param source2 — The second source object from which to copy properties.
+
+const obj3 = {...obj1, ...obj2};
+// console.log(obj3);
+
+// Object.keys(object-name) //will return the array of keys and same the values : Object.values(object-name)
+
+console.log(Object.entries(tinderUser)); // every key-value formed into array
+console.log(tinderUser.hasOwnProperty('isLoggedIn')); //to check if that property exist?
