075111e25a580396fdae914b5ac5151500989b01
Chhavi Pandey <pandeychhavi29@gmail.com>
2025-04-14 08:33:24 +0530

    string

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -3,13 +3,14 @@ const codeRepo = 5;
 
 // console.log(name + codeRepo); // it's old fashioned and not preferable
 
-console.log(`Hello my name is ${name} and I have ${codeRepo} code repos on github`) // comparatively better and also called STRING INTERPOLOATION
+console.log(`Hello my name is ${name} and I have ${codeRepo} code repos on github`) // comparatively better and also called STRING INTERPOLOATION :- In sting interpolation we create placeholders where we can inject variable
 
 const gameName = "HopeKlausMikaelson";
 
 // console.log(gameName[0]);
 
-// console.log(gameName.__proto__);
+// console.log(gameName.__proto__); // access to prototype of this string
+console.log(gameName.length);
 
 const gameNameNew = new String("hope-kl")
 
@@ -22,19 +23,21 @@ console.log(gameName.indexOf('M'));
 const newString = gameName.substring(0,5); //In substring method we must not use -ve index value as it doesn't consider
 console.log(newString);
 
-const anotherString = gameName.slice(-16,5)
+const anotherString = gameName.slice(-18,5)
 console.log(anotherString);
 
 
 const newStringOne = "     Klaus    ";
 console.log(newStringOne);
-console.log(newStringOne.trim()); // trim.start and trim.end also exist
+console.log(newStringOne.trim()); // trim.start and trim.end also exist. it also works on whitespaces and terminate line
 
 
 const url = "https://theOriginal.com/klaus%20Mikaelson";
 
 console.log(url.replace('%20', '-'))
 
-// url.includes() will help with finding the given string is exist or not. It is also a string method
+// url.includes() : includes() will help with finding the given string is exist or not. It is also a string method
 
 // Go through with all of the methods of string in js. on console you will get the list
+
+console.log(gameName.split('-')); // What does it do?
