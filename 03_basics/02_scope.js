4633726f17b66d2a96c52b385333839e576214a5
Chhavi Pandey <pandeychhavi29@gmail.com>
2025-04-18 21:41:32 +0530

    scope

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,20 @@
+// let a = 10;
+// const b = 20;
+// var c = 30;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+var c = 300;
+if (true) {
+    let a = 10;
+    const b = 20;
+    var c = 30;
+}
+
+    // console.log(a);
+    // console.log(b);
+    console.log(c);
+
+    // var: global scope
+    // let and const : block or local scope
