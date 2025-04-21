21d31bd271c5a00b822a88fb4e8d4e0285d414d2
Chhavi Pandey <pandeychhavi29@gmail.com>
2025-04-21 15:23:52 +0530

    while loop done

---
+++ 05_Iterations/02_whileLoop.js
@@ -0,0 +1,29 @@
+// let i = 1
+
+// while (i <= 10) {
+//     console.log(`i : ${i} \n`) ;
+//     i++;
+// }
+
+let vamp = ['Damon', 'Stefan', 'Caroline', 'Jenna', 'Elena'];
+
+let arr = 0;
+
+// while (arr < vamp.length) {
+//     // console.log(`Vamps are ${vamp[arr]}`);
+//     arr = arr + 1;
+// }
+
+// let score = 1; 
+
+// do{
+//     console.log(`Score is ${score}`);
+//     score++;
+// }while(score <= 10);
+
+let score = 11; 
+
+do{
+    console.log(`Score is ${score}`);
+    score++;
+}while(score <= 10);
